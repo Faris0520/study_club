5f3712eb71f0b07ae5f85d33cc96afcac82262d6
Faris Daffa <faris.6dsdiaf@gmail.com>
2025-08-12 12:00:45 +0700

    day 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,316 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void
+main() {
+  runApp(
+    const MyApp(),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp
+    extends
+        StatelessWidget {
+  const MyApp({
+    super.key,
+  });
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(
+    BuildContext context,
+  ) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 255, 111, 0),
+          seedColor: const Color.fromARGB(
+            255,
+            7,
+            204,
+            234,
+          ),
         ),
       ),
-      home: const MyHomePage(title: 'Selamat datang di aplikasi saya'),
+      home: const MyHomePage(
+        title: 'Study Club',
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
+class MyHomePage
+    extends
+        StatefulWidget {
+  const MyHomePage({
+    super.key,
+    required this.title,
+  });
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<
+    MyHomePage
+  >
+  createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState
+    extends
+        State<
+          MyHomePage
+        > {
+  int _selectedIndex = 0;
+
   int _counter = 0;
 
   void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+    setState(
+      () {
+        _counter++;
+      },
+    );
+  }
+
+  // Daftar halaman
+  final List<
+    Widget
+  >
+  _pages = [
+    const Center(
+      child: Text(
+        "Beranda",
+        style: TextStyle(
+          fontSize: 24,
+        ),
+      ),
+    ),
+    ProfilePage(),
+    const Center(
+      child: Text(
+        "Halaman Aktivitas",
+        style: TextStyle(
+          fontSize: 24,
+        ),
+      ),
+    ),
+    const Center(
+      child: Text(
+        "Pengaturan",
+        style: TextStyle(
+          fontSize: 24,
+        ),
+      ),
+    ),
+  ];
+
+  void _onItemTapped(
+    int index,
+  ) {
+    setState(
+      () {
+        _selectedIndex = index;
+      },
+    );
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(
+    BuildContext context,
+  ) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            const Text('hi'),
-          ],
+        backgroundColor: Theme.of(
+          context,
+        ).colorScheme.inversePrimary,
+        centerTitle: true,
+        title: Text(
+          widget.title,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            fontFamily: "Google Sans",
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.restart_alt),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+            ),
+            label: "Beranda",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person,
+            ),
+            label: "Profile",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.list,
+            ),
+            label: "Aktivitas",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.settings,
+            ),
+            label: "Pengaturan",
+          ),
+        ],
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
+    );
+  }
+}
+
+// Halaman Profile
+class ProfilePage
+    extends
+        StatelessWidget {
+  @override
+  Widget build(
+    BuildContext context,
+  ) {
+    void dialog(
+      BuildContext context,
+    ) {
+      showDialog(
+        context: context,
+        builder:
+            (
+              BuildContext context,
+            ) {
+              return AlertDialog(
+                title: const Text(
+                  "Data berhasil diubah!",
+                ),
+                content: const Text(
+                  "Data telah disimpan.",
+                ),
+                actions: [
+                  TextButton(
+                    onPressed: () => Navigator.of(
+                      context,
+                    ).pop(),
+                    child: const Text(
+                      "Tutup",
+                    ),
+                  ),
+                ],
+              );
+            },
+      );
+    }
+
+    return Padding(
+      padding: const EdgeInsets.all(
+        25,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+            child: Column(
+              children: [
+                const Text(
+                  'Profile',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontFamily: 'Google Sans',
+                    fontWeight: FontWeight.bold,
+                    height: 2,
+                  ),
+                ),
+                const CircleAvatar(
+                  backgroundImage: AssetImage(
+                    "Assets/Image/miyabu.png",
+                  ),
+                  radius: 71,
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            "Nama",
+            style: TextStyle(
+              fontSize: 17,
+              color: Colors.grey,
+            ),
+          ),
+          const Text(
+            "Faris Daffa",
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+          const Divider(),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            "Alamat",
+            style: TextStyle(
+              fontSize: 17,
+              color: Colors.grey,
+            ),
+          ),
+          const Text(
+            "Mont Esus East",
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+          const Divider(),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            "About Me",
+            style: TextStyle(
+              fontSize: 17,
+              color: Colors.grey,
+            ),
+          ),
+          const Text(
+            "About what?",
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+          const Divider(),
+          const SizedBox(
+            height: 20,
+          ),
+
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(
+                255,
+                18,
+                120,
+                203,
+              ),
+            ),
+            onPressed: () => dialog(
+              context,
+            ),
+            child: const Text(
+              "Update Profile",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 17,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
